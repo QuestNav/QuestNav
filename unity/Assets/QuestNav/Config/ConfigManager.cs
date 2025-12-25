7ad0bf0faf33fc2535d8d09913d26d7bb33fef96
FunkyChicken <sean.ernstes@proton.me>
2025-12-25 16:16:12 -0500

    style: c# headers for copyright

--- unity/Assets/QuestNav/Config/ConfigManager.cs
+++ unity/Assets/QuestNav/Config/ConfigManager.cs
@@ -1,3 +1,22 @@
+// QUESTNAV
+// https://github.com/QuestNav
+// Copyright (C) 2026 QuestNav
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+// This file is part of QuestNav.
+//
+// QuestNav is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// QuestNav is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with QuestNav. If not, see https://www.gnu.org/licenses/.
 using System;
 using System.IO;
 using System.Threading;

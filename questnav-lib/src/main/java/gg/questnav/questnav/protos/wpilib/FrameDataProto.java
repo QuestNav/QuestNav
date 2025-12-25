97a3eb9b19bf74e28d7c1c6de236e99ff0178c40
FunkyChicken <sean.ernstes@proton.me>
2025-12-25 15:12:25 -0500

    Add new license and update Java headers

--- questnav-lib/src/main/java/gg/questnav/questnav/protos/wpilib/FrameDataProto.java
+++ questnav-lib/src/main/java/gg/questnav/questnav/protos/wpilib/FrameDataProto.java
@@ -1,11 +1,24 @@
 /*
-* QUESTNAV
-  https://github.com/QuestNav
-* Copyright (C) 2025 QuestNav
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the MIT License as published.
-*/
+ * QUESTNAV
+ * https://github.com/QuestNav
+ * Copyright (C) 2025 QuestNav
+ *
+ * This file is part of QuestNav.
+ *
+ * QuestNav is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QuestNav is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with QuestNav. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package gg.questnav.questnav.protos.wpilib;
 
 import edu.wpi.first.util.protobuf.Protobuf;
